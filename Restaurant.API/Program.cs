c3022ef48cc9fd37e847deff705d3f275a808b5f
pokr003 <pokrivchakzvitalja@gmail.com>
2024-08-15 18:17:51 +0300

    setup database

--- Restaurant.API/Program.cs
+++ Restaurant.API/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurant.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RestaurantDbContext>(options =>
+    options
+        .UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"))
+        .UseSnakeCaseNamingConvention()
+);
 
 var app = builder.Build();
 
