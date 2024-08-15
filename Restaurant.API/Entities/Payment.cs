c8be7c0fc85a846f030c4a8dad8c5c542317f0d0
pokr003 <pokrivchakzvitalja@gmail.com>
2024-08-15 18:17:08 +0300

    add some changes in entities

--- Restaurant.API/Entities/Payment.cs
+++ Restaurant.API/Entities/Payment.cs
@@ -2,8 +2,9 @@ namespace Restaurant.API.Entities;
 
 public sealed class Payment : Entity
 {
-    public required Order Order { get; set; }
     public PaymentStatus Status { get; set; }
     public decimal Bill { get; set; }
-    public decimal Tip { get; set; }
+    public decimal? Tip { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
--- Restaurant.API/Entities/Product.cs
+++ Restaurant.API/Entities/Product.cs
@@ -6,6 +6,6 @@ public sealed class Product : Entity
     public required string Description { get; set; }
     public required string ImageUrl { get; set; }
     public decimal? OldPrice { get; set; } = 0.00M;
-    public decimal NewPrice { get; set; }
+    public decimal Price { get; set; }
     public required ProductCategory Category { get; set; }
 }
